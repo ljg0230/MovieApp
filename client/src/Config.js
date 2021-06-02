e8f3b81839625cfc2296ea4697d1254561b9dbd3
lee <ljg0230@gmail.com>
2021-06-02 23:17:39 +0900

    Landing Page 및 Detail Page 기초 작업

---
+++ client/src/Config.js
@@ -0,0 +1,7 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+export const API_KEY = 'eafff199594c4a250149ac45a81d0831';
